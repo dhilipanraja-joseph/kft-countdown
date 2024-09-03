482550eadadea49a7079f987699f9974bb9c78d6
Dhilipanraja Joseph <dhilipanjoseph@gmail.com>
2024-09-02 22:25:31 -0500

    start countdown

--- src/Components/StopWatch.js
+++ src/Components/StopWatch.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { useStopwatch } from 'react-timer-hook';
+import { useTimer } from 'react-timer-hook';
 import TimerStyled from './TimerStyled';
 
 function MyStopwatch() {
-    function getDiffinSec(dt1, dt2) {
-        const diffMs = dt2 - dt1;
-        const diffSec = Math.abs(diffMs / 1000);
-        return diffSec;
-      }
+    // function getDiffinSec(dt1, dt2) {
+    //     const diffMs = dt2 - dt1;
+    //     const diffSec = Math.abs(diffMs / 1000);
+    //     return diffSec;
+    //   }
 
 
-    const startDate = new Date('09/1/2024');
+    const endDate = new Date('10/19/2024');
 
     const stopwatchOffset = new Date() 
     
-    stopwatchOffset.setSeconds(stopwatchOffset.getSeconds() + getDiffinSec(new Date(), startDate));
+    // stopwatchOffset.setSeconds(stopwatchOffset.getSeconds() + getDiffinSec(new Date(), startDate));
     
     const {
         seconds,
         minutes,
         hours,
         days,
-    } = useStopwatch({ autoStart: true, offsetTimestamp: stopwatchOffset});
+    } = useTimer({expiryTimestamp: endDate});
 
 
     return (
