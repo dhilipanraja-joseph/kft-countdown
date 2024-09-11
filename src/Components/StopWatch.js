15fa02991fe13367e2434dca1ded3175094c34a2
Dhilipanraja Joseph <dhilipanjoseph@gmail.com>
2024-09-11 06:51:12 -0500

    Update StopWatch.js

--- src/Components/StopWatch.js
+++ src/Components/StopWatch.js
@@ -4,7 +4,7 @@ import TimerStyled from './TimerStyled';
 
 function MyStopwatch() {
 
-    const endDate = new Date('09/10/2024');
+    const endDate = new Date('09/13/2024');
 
     const stopwatchOffset = new Date() 
         
